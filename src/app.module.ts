cd408150551f19b46b135913172605b838811724
박민한 <runningwater@optatumplatform.com>
2020-12-07 14:12:12 +0900

    add typeorm pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5431,
+      username: 'postgres',
+      password: 'pserts1!o',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: false,
     }),
     RestaurantModule,
   ],
