40e5a652d35e26760f05084c05b741041f3fc4d7
박민한 <runningwater@optatumplatform.com>
2020-12-30 10:26:43 +0900

    add createAccount e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,12 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
@@ -23,9 +29,60 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
+  describe('createAccount', () => {
+    const EMAIL = 'running@water.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "12345",
+            role: Owner
+          }){
+            ok,
+            error,
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+    it('shound fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "12345",
+            role: Owner
+          }){
+            ok,
+            error,
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            '기존에 존재하는 이메일입니다.',
+          );
+        });
+    });
+  });
+
   it.todo('me');
   it.todo('userProfile');
-  it.todo('mcreateAccounte');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
