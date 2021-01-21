c30865c3ccbcfe670682e4f07b3a895a899083b5
Running water <alsgks1218@naver.com>
2021-01-22 00:00:24 +0900

    12.12

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PubSub } from 'graphql-subscriptions';
 import {
   NEW_COOKED_ORDER,
+  NEW_ORDER_UPDATE,
   NEW_PENDING_ORDER,
   PUB_SUB,
 } from 'src/common/common.constants';
@@ -208,9 +209,7 @@ export class OrderService {
     { id: orderId, status }: EditOrderInput,
   ): Promise<EditOrderOutput> {
     try {
-      const order = await this.orders.findOne(orderId, {
-        relations: ['restaurant'],
-      });
+      const order = await this.orders.findOne(orderId);
       if (!order) {
         return {
           ok: false,
@@ -248,15 +247,16 @@ export class OrderService {
           error: '주문을 수정할 수 없습니다.',
         };
       }
-
-      await this.orders.save([{ id: orderId, status }]);
+      const newOrder = { ...order, status };
+      await this.orders.save({ id: orderId, status });
       if (user.role === UserRole.Owner) {
         if (status === OrderStatus.Cooked) {
           await this.pubsub.publish(NEW_COOKED_ORDER, {
-            cookedOrders: { ...order, status },
+            cookedOrders: newOrder,
           });
         }
       }
+      await this.pubsub.publish(NEW_ORDER_UPDATE, { orderUpdates: newOrder });
       return {
         ok: true,
       };
