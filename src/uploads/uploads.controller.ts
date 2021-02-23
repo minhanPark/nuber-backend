e2490b79f6c672d8f4d5d9072c52b6d801700582
Running water <alsgks1218@naver.com>
2021-02-23 23:11:56 +0900

    add aws

---
+++ src/uploads/uploads.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk/';
+
+const BUCKET_NAME = 'runningwaternubereats';
+
+@Controller('uploads')
+export class UploadsController {
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: 'AKIAXIZLVBXZSNMFWV5C',
+        secretAccessKey: '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',
+      },
+    });
+    try {
+      const objectName = `${Date.now() + '' + file.originalName}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (e) {
+      return null;
+    }
+  }
+}
